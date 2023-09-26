744e2540138fcc84199ea45a38961fbe71227982
Ezra Machoka <ezra.machoka@student.moringaschool.com>
2023-09-26 13:14:26 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
+        
 
     def load_json(self):
-        pass
+        pyth_list = []
+        data = json.loads(self.get_response_body())
+        for x in data:
+            pyth_list.append(x)
+
+        return pyth_list
+
+        
+if __name__ == '__main__':
+    program = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+    print(program.load_json())
